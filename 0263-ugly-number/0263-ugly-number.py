91abc3a9e53af05f38a3840eb56dc23eb1f8c047
Diljot Singh <83709039+Diljot11@users.noreply.github.com>
2022-11-19 00:07:31 +0530

    Time: 21 ms (88.41%), Space: 13.3 MB (88.82%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -1,10 +1,23 @@
 class Solution(object):
     def isUgly(self, n):
-        
-        for i in (2,3,5):
-            while n%i==0<n:
-                n/=i
+        if n ==0:
+            return False
+        while n%2==0:
+            n/=2
+            # print n
+        while n%3==0:
+            n/=3
+            # print n
+        while n%5==0:
+            n/=5
+        # print n
         return n==1
+
+        
+#         for i in (2,3,5):
+#             while n%i==0<n:
+#                 n/=i
+#         return n==1
         
         
         
