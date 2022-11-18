a920b4446fbe322c44f1aced0e2ee92af6e3b77f
Diljot Singh <83709039+Diljot11@users.noreply.github.com>
2022-11-18 23:58:04 +0530

    Time: 40 ms (36.38%), Space: 13.6 MB (15.85%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isUgly(self, n):
+        
+        for i in (2,3,5):
+            while n%i==0<n:
+                n/=i
+        return n==1
+        
+        
+        
+        
+        
+        """
+        :type n: int
+        :rtype: bool
+        """
+        
